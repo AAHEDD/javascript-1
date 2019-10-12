31ced2e044cb4270c5684e05f419642a703233b9
Ahed <ahed.hafez@gmail.com>
2019-10-12 19:14:42 +0200

    week2 javascript project Ahed

--- week-2-project/scripts/caesarize.js
+++ week-2-project/scripts/caesarize.js
@@ -19,6 +19,72 @@ const caesarizeTests = [
 ];
 function caesarize(str, shiftNum) {
   // write me!
+        let src = str.split(''), ctr1 = '' ;
+        let splChars = "^*|,\":<>[]{}`\';()@&$#%\\\/";
+        let letters = /[A-Za-z]/;
+        let space= " ";
+        let caesarized = '' ;
+        let result = '';
+        let charcode = 0;
+
+
+        
+
+          for (var x = 0; x < src.length; x++) 
+          {
+
+             if(src[x].match(space)) {
+            
+              ctr1 = ' ';
+              
+            
+             }
+
+            else if(src[x].match(letters))
+            {
+              if (src[x] == src[x].toUpperCase()) {
+                ctr1 = src[x].toLowerCase();
+               }
+               else if(src[x] == src[x].toLowerCase()){
+                ctr1 = src[x].toUpperCase();
+               }
+
+            }
+
+          else if (splChars.indexOf(src[x] ) != -1) {
+              
+              ctr1 = ' ';
+
+          } 
+
+          else if(!isNaN(src[x])) {
+            
+              
+              ctr1 = ' ';
+            
+          }
+
+          result += ctr1;
+            ctr1 = '';
+
+          }
+          
+          for (var z = 0; z < result.length; z++) {
+            if (result[z].match(letters)){
+              charcode = result[z].charCodeAt() + shiftNum;
+              
+              caesarized += result[z].fromCharCode(charcode);
+                
+                y=true;
+                
+            } else if (result[z] == ' ' && y==true) {
+              caesarized += ' ';
+              y=false;
+            }
+          }
+          
+            
+         return caesarized;
 }
 evaluate(caesarize, caesarizeTests);
 
@@ -46,6 +112,9 @@ function caesarizeHandler() {
   console.log('strToCaesarize:', typeof strToCaesarize, ',', strToCaesarize);
   console.log('shiftNumber:', typeof shiftNumber, ',', shiftNumber);
   console.log('caesarized:', typeof caesarized, ',', caesarized);
+  
+  
+  
 };
 const caesarizeButton = document.getElementById('caesarize-button');
 caesarizeButton.addEventListener('click', caesarizeHandler);
